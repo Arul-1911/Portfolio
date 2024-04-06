26b0b4eddff16699b13c59da77038449306de238
Arul-1911 <arulpraveen19112002@gmail.com>
2024-04-06 15:52:08 +0530

    completed experience section

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styles from './experience.module.css';
+import skills from '../../data/skills.json';
+import experience from '../../data/history.json';
+import { imageUrl } from '../../utils';
+
+function Experience() {
+  return (
+    <section className={styles.container} id='experience'>
+      <h2 className={styles.title}>Experience</h2>
+      <div className={styles.content}>
+         <div className={styles.skills}>{
+            skills.map((skill,id) => {
+               return(
+                  <div key={id} className={styles.skill}>
+                     <div className={styles.skillImageContainer}>
+                        <img src={imageUrl(skill.imageSrc)} alt="skills-img" />
+                     </div>
+                     <p>{skill.title}</p>
+                  </div>             
+                   )
+            })
+            }</div>
+         <ul className={styles.history}>
+            {
+               experience.map((history,id) => {
+                  return (
+                    <li key={id} className={styles.historyItem}>
+                      {/* <img
+                        src={imageUrl(history.imageSrc)}
+                        alt={`${history.title} logo`}
+                      /> */}
+                      <div className={styles.historyItemDetails}>
+                        <h3>{`${history.role}, ${history.organisation}`}</h3>
+                        <p>{`${history.startDate} - ${history.endDate}`}</p>
+                        <ul>{history.experiences.map((experience,id) => {
+                              return(
+                                 <li key={id}>{experience}</li>
+                              )
+                        })}</ul>
+                      </div>
+                    </li>
+                  );
+               })
+            }
+         </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Experience
