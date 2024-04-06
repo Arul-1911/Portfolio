398a300ea3d9b72b0ecbe6893073471afc0b6721
Arul-1911 <arulpraveen19112002@gmail.com>
2024-04-06 21:21:47 +0530

    project section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
+import Project from "./components/projects/Project";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Hero/>
         <About/>
         <Experience/>
+        <Project/>
       </h1>
     </>
   );
---
+++ src/components/projects/Project.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from './project.module.css';
+import projects from '../../data/projects.json';
+import { imageUrl } from '../../utils';
+
+function Project() {
+  return (
+    <section className={styles.container} id='projects'>
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.Projects}>{
+         projects.map((project,id)=> {
+            return (
+              <div key={id} className={styles.cardContainer}>
+                <img
+                  src={imageUrl(project.imageSrc)}
+                  alt={project.title}
+                  className={styles.cardImage}
+                />
+                <h3 className={styles.cardTitle}>{project.title}</h3>
+                <p className={styles.cardDescription}>{project.description}</p>
+                <ul className={styles.skills}>
+                  {project.skills.map((skill, id) => {
+                    return (
+                      <li key={id} className={styles.skill}>
+                        {skill}
+                      </li>
+                    );
+                  })}
+                </ul>
+                <div className={styles.links}>
+                  <a href={project.demo} className={styles.link}>
+                    Demo
+                  </a>
+                  <a href={project.source} className={styles.link}>
+                    Source
+                  </a>
+                </div>
+              </div>
+            );
+         })
+         }
+         </div>
+    </section>
+  )
+}
+
+export default Project
